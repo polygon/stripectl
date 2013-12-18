0d739829c3ab40e898514a4cdeaa7ce80f1296fb
Jan Dohl <jan@legxaro.de>
2013-12-19 00:33:44 +0100

    Working Knight Rider light effect

--- main.c
+++ main.c
@@ -12,6 +12,8 @@
 */
 
 #define NUM_LEDS 240
+#define STARTLED 60
+#define STOPLED 170
 
 #include <stdio.h>
 
@@ -94,6 +96,10 @@ int main(void) {
 	LPC_IOCON->TRST_PIO0_14  &= ~0x07;
 	LPC_IOCON->TRST_PIO0_14  |= 0x01;
 
+    // Disable pull-up on LED Stripe pin (default enabled)
+    LPC_IOCON->TDI_PIO0_11 &= ~0x1F;
+    LPC_IOCON->TDI_PIO0_11 |= 0x01;
+
     //data direction: output
 	LPC_GPIO->DIR[0] |= (1<<12);
 	LPC_GPIO->DIR[0] |= (1<<14);
@@ -103,9 +109,12 @@ int main(void) {
 
     for (i = 0; i < NUM_LEDS; i++)
     {
-        led_data[i][0] = 0;
-        led_data[i][1] = 32;
-        led_data[i][2] = 0;
+        if (!(i % 10))
+        {
+            led_data[i][0] = 0;
+            led_data[i][1] = 0;
+            led_data[i][2] = 0;
+        }
     }
     LPC_GPIO->B0[12] = 0;
     output_stripe_data();
@@ -115,13 +124,57 @@ int main(void) {
 	buttons_init();
 	buttonsInitialized=1;
     buttons_get_press( KEY_C|KEY_B|KEY_A );
+    int pos = STARTLED;
+    int dir = 4;
+    int ended = 0;
 
     while (1)
     {
-        /*char a = 89;
-        UARTSend(&a, 1);*/
-        delay_ms(100);
-        LPC_GPIO->B0[12] = 1 - LPC_GPIO->B0[12];
+        if (ended)
+        {
+            for (i = 0; i < NUM_LEDS; i++)
+            {
+                led_data[i][1] = 0;
+            }
+            output_stripe_data();
+            delay_ms(100);
+            ended = 0;
+            for (i = 0; i < NUM_LEDS; i++)
+            {
+                led_data[i][1] = 0;
+            }
+            output_stripe_data();
+            delay_ms(500);
+
+        }
+
+        for (i = 0; i < NUM_LEDS; i++)
+        {
+            led_data[i][1] = 0;
+        }
+        led_data[pos][1] = 4;
+        led_data[pos+1][1] = 8;
+        led_data[pos+2][1] = 16;
+        led_data[pos+3][1] = 32;
+        led_data[pos+4][1] = 64;
+        led_data[pos+5][1] = 128;
+        led_data[pos+6][1] = 255;
+        led_data[pos+7][1] = 128;
+        led_data[pos+8][1] = 64;
+        led_data[pos+9][1] = 32;
+        led_data[pos+10][1] = 16;
+        led_data[pos+11][1] = 8;
+        led_data[pos+12][1] = 4;
+        pos += dir;
+        if (pos >= STOPLED-1)
+            dir = -6;
+        if (pos <= STARTLED-6)
+        {
+            ended = 1;
+            dir = 6;
+            pos = STARTLED;
+        }
+        output_stripe_data();
     }
 }
 
